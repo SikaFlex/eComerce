d13c68d043d523d85dd67f3ef659206a5533f63d
angel.garcia <angel.garcia@axisdata.net>
2024-02-26 12:41:52 +0100

    Loggin and stores

--- angel_ecomerce/app/(root)/page.tsx
+++ angel_ecomerce/app/(root)/page.tsx
@@ -1,10 +1,28 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store.modal";
+import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state)=> state.onOpen);
+  const isOpen = useStoreModal((state)=> state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen) onOpen();
+  
+
+  }, [isOpen, onOpen])
+  
+
+
     return (
-      <div>
-        <Button size='default' variant='outline'>Click</Button>
+      <div className="p-4">
+        Root page
       </div>
     );
   }
   
+  export default SetupPage;
