ec4dcd16db4f3298deae11fbe152dc8d8ad0f105
angel.garcia <angel.garcia@axisdata.net>
2024-02-28 11:14:14 +0100

    solved routes failed

---
+++ angel_ecomerce/app/(root)/(routes)/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+
+import { useStoreModal } from "@/hooks/use-store.modal";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return (
+    <div>ENSEÑATE ALGO MANIN</div>
+  )
+};
+ 
+export default SetupPage;
